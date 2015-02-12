d68e3bf3161340f227ca697eeb39f8c52cc006eb
cognar <connor.mccloskey@gmail.com>
2015-02-11 17:54:41 -0800

    Update gameoflife.py

--- gameoflife.py
+++ gameoflife.py
@@ -3,49 +3,37 @@ import time
 from random import randint
 
 def next_gen():
-    n = int(raw_input('width:'))
-    m = int(raw_input('height:'))
+    m = int(raw_input('width:'))
+    n = int(raw_input('height:'))
     genLim = int(raw_input('number of generations: '))
     speed = float(raw_input('speed: '))
-    cells = [[randint(0,1) for i in xrange(n)] for k in xrange(m)]
+    cells = [[randint(0,1) for i in xrange(m)] for k in xrange(n)]
     for i in xrange(0,genLim):
         next = [[0 for i in row] for row in cells]
         printfriend = [[0 for i in row] for row in cells]
         for i in xrange(0,n):
             for k in xrange(0,m):
-                if i == 0:
-                    if k == 0:
-                        next[i][k] = cells[i+1][k] + cells[i+1][k+1] + cells[i][k+1]
-                    elif k == m-1:
-                        next[i][k] = cells[i+1][k] + cells[i+1][k-1] + cells[i][k-1]
-                    else:
-                        next[i][k] = cells[i+1][k] + cells[i+1][k-1] + cells[i][k-1] + cells[i][k+1] + cells[i+1][k+1]
-                elif i == n-1:
-                    if k == 0:
-                        next[i][k] = cells[i-1][k] + cells[i-1][k+1] + cells[i][k+1]
-                    elif k == m-1:
-                        next[i][k] = cells[i-1][k] + cells[i-1][k-1] + cells[i][k-1]
-                    else:
-                        next[i][k] = cells[i-1][k] + cells[i-1][k-1] + cells[i][k-1] + cells[i][k+1] + cells[i-1][k+1]
-                elif k == 0:
-                    next[i][k] = cells[i-1][k] + cells[i+1][k] + cells[i-1][k+1] + cells[i][k+1] + cells[i-1][k+1]
-                elif k == m-1:
-                    next[i][k] = cells[i-1][k] + cells[i+1][k] + cells[i-1][k-1] + cells[i][k-1] + cells[i-1][k-1]
-                else:
-                    next[i][k] = cells[i-1][k] + cells[i-1][k-1] + cells[i][k-1] + cells[i][k+1] + cells[i-1][k+1] + cells[i+1][k] + cells[i+1][k-1] + cells[i+1][k+1]
+                next[i][k] = cells[-1+(i%n)][k%m] + cells[-1+(i%n)][-1+(k%m)] + cells[i%n][-1+(k%m)] + cells[i%n][(k+1)%m] + cells[-1+(i%n)][(k+1)%m] + cells[(i+1)%n][k%m] + cells[(i+1)%n][-1+(k%m)] + cells[(i+1)%n][(k+1)%m]
+        for i in xrange(n):
+            for k in xrange(m):
+                if cells[i][k] == 1:
+                    if next[i][k] < 2:
+                        next[i][k] = 0    # underpopularion
+        for i in xrange(n):
+            for k in xrange(m):
+                if cells[i][k] == 1:
+                    if next[i][k] == 2 or next[i][k] == 3:
+                        next[i][k] = 1      # Stasis
+        for i in xrange(n):
+            for k in xrange(m):
+                    if next[i][k] > 3:
+                        next[i][k] = 0      # overpopulation
         for i in xrange(n):
             for k in xrange(m):
                 if cells[i][k] == 0:
                     if next[i][k] == 3:
                         next[i][k] = 1
-                    else: next[i][k] = 0
-                elif cells[i][k] == 1:
-                    if next[i][k] < 2:
-                        next[i][k] = 0
-                    elif next[i][k] == 2 or next[i][k] == 3:
-                        next[i][k] = 1
-                    elif next[i][k] > 3:
-                        next[i][k] = 0
+                    else: next[i][k] = 0    # reproduction
         cells = next
         for i in xrange(n):
             for k in xrange(m):
@@ -54,7 +42,7 @@ def next_gen():
 
         os.system('cls' if os.name == 'nt' else 'clear')
         print "\n\n\n"
-        for i in printfriend: print '  '.join(i), "\n"
+        for i in printfriend: print ''.join(i)
         time.sleep(1/speed)
     return 0
 
